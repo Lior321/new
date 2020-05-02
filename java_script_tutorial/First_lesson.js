9a8f987f2cb9fe607e25b101cbdf5fe1baea8808
Lior321 <liorperez77@gmail.com>
2020-05-02 17:52:35 +0300

    a

---
+++ java_script_tutorial/First_lesson.js
@@ -0,0 +1,36 @@
+/* data types:
+undefined
+null - NONE
+boolean
+string
+symbol
+number
+object */
+
+var a = 1; //can change
+let b = 1; 
+const c = 1; //can't change
+
+var a; // אפשר ליצור משתנה בלי להגדיר למה הוא שווה
+a = 2;
+a = Boolean(a==b)
+console.log(a)
+
+/*
+Code  Output
+\'    single quote
+\"    double quote
+\\    backslash
+\n    new line
+\r    carriage return
+\t    tab
+\b    backspace
+\f    form feed
+*/
+
+var my_str = "this is a string";
+var str_len = my_str.length;
+console.log(str_len)
+
+// you can't change an individual letter in strings :(
+
